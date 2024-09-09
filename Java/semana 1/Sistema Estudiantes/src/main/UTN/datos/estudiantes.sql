0fc7ebc7e143cddf915088c15552ef5d80d5c393
Juan Schallmoser <schallmoserjuan@gmail.com>
2024-09-09 17:35:57 -0300

    Semana 1

---
+++ Java/semana 1/Sistema Estudiantes/src/main/UTN/datos/estudiantes.sql
@@ -0,0 +1,26 @@
+SELECT * FROM estudiantes2022;
+insert into estudiantes2022(nombre, apellido , telefono, email) values ("juan", "perez","2614545456","juan@gmail.com");
+update estudiantes2022 set nombre="juan carlos", apellido="garcia" where idestudiantes2022=1;
+delete from estudiantes2022 where idestudiantes2022=3;
+alter table estudiantes2022 auto_increment=1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
