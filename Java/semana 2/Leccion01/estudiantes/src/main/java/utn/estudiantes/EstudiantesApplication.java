5ad34f73d4e077c0406e0fed9010752af0667295
Juan Schallmoser <schallmoserjuan@gmail.com>
2024-09-12 16:57:59 -0300

    clase 05 Java

--- Java/semana 2/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/semana 2/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -7,12 +7,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import utn.estudiantes.servicio.EstudianteServicio;
+import utn.estudiantes.modelo.Estudiantes2022;
+
+import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class EstudiantesApplication implements CommandLineRunner {
 	@Autowired
 	private EstudianteServicio estudianteServicio;
-	private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
+	private static final Logger logger =
+			LoggerFactory.getLogger(EstudiantesApplication.class);
 
 	String nl = System.lineSeparator();
 
@@ -26,5 +31,26 @@ public class EstudiantesApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		logger.info(nl+"Ejecutando el metodo run de Spring..."+nl);
+		var salir = false;
+		var consola = new Scanner(System.in);
+		while(!salir){
+			mostrarMenu();
+			salir = ejecutarOpciones(consola);
+			logger.info(nl);
+		}
+	}
+
+	private void mostrarMenu(){
+		logger.info(nl);
+		logger.info("""
+				**** Sistema de Estudiantes ****
+				1. Listar Estudiantes
+				2. Busacar Estudiantes
+				3. Agregar Estudiante
+				4. Modificar Estudiante
+				5. Eliminar Estudiante
+				6. Salir
+				Elija una opcion:
+				""");
 	}
 }
