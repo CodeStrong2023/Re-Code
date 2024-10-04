44b7688924779b85e00aab7e3af154660779aa0e
ivo <rossiivomartin@gmail.com>
2024-10-04 19:02:48 -0300

    avanzo el sistema de estudiantes hasta carga de productos clase 1

---
+++ JavaScript/estudiantes2024/client/js/product.js
@@ -0,0 +1,58 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Zapatillas",
+        price: 69,
+        quanty: 1,
+        img: "media/zapatillas.jpg"
+    },
+    {
+        id: 2,
+        productName: "Hacha",
+        price: 58,
+        quanty: 1,
+        img: "media/hacha.jpg"
+    },
+    {
+        id: 3,
+        productName: "Neumatico Dunlop",
+        price: 96,
+        quanty: 1,
+        img: "media/rueda.jpg"
+    },
+    {
+        id: 4,
+        productName: "Control Joystick PS5",
+        price: 99,
+        quanty: 1,
+        img: "media/control.jpg"
+    },
+    {
+        id: 5,
+        productName: "Mochila",
+        price: 14,
+        quanty: 1,
+        img: "media/mochila.jpg"
+    },
+    {
+        id: 6,
+        productName: "Celular Chamsun 22",
+        price: 140,
+        quanty: 1,
+        img: "media/celular.jpg"
+    },
+    {
+        id: 7,
+        productName: "Cartera",
+        price: 12,
+        quanty: 1,
+        img: "media/cartera.jpg"
+    },
+    {
+        id: 8,
+        productName: "Bolso",
+        price: 9,
+        quanty: 1,
+        img: "media/bolso.jpg"
+    },
+];
