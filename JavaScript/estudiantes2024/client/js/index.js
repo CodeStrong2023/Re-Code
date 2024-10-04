fd8dc7f877a46f4698f91dc0a5c9d4aae1c72e35
ivo <rossiivomartin@gmail.com>
2024-10-04 20:14:57 -0300

    completo clase2 sistema estudiantes

--- JavaScript/estudiantes2024/client/js/index.js
+++ JavaScript/estudiantes2024/client/js/index.js
@@ -1,12 +1,40 @@
 const shopContent = document.getElementById("shopContent");
+const cart = []; // Carrito vacío
+
 
 productos.forEach((product) => {
-    const content = document.createElement("div");
-    content.innerHTML = `
+  const content = document.createElement("div");
+  content.innerHTML = `
     <img src="${product.img}">
     <h3>${product.productName}</h3>
-    <p class="price">${product.price} $</p>
-    `;
-    shopContent.append(content);
+    <p style="color:red;font-weight:bolder;">$${product.price}</p>
+  `;
+  shopContent.append(content);
 
+  const buyButton = document.createElement("button");
+  buyButton.innerText = "Comprar";
+
+  content.append(buyButton);
+
+  buyButton.addEventListener("click", () => {
+    const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+
+    if (repeat) {
+      cart.map((prod) => {
+        if (prod.id === product.id) {
+          prod.quanty++;
+          displayCartCounter();
+        }
+      });
+    } else {
+      cart.push({
+        id: product.id,
+        productName: product.productName,
+        price: product.price,
+        quanty: 1, 
+        img: product.img,
+      });
+      displayCartCounter();
+    }
+  });
 });
