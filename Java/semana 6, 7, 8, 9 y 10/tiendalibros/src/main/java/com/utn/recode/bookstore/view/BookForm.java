6459d77378a745d4f71755af86ae026d23147c50
ivo <rossiivomartin@gmail.com>
2024-10-16 20:59:28 -0300

    clase 8 bookstore add books

--- Java/semana 6, 7, 8, 9 y 10/tiendalibros/src/main/java/com/utn/recode/bookstore/view/BookForm.java
+++ Java/semana 6, 7, 8, 9 y 10/tiendalibros/src/main/java/com/utn/recode/bookstore/view/BookForm.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class BookForm extends JFrame {
     BookService bookService;
     private JPanel panel;
+
     private JTable bookBoard;
 
     private DefaultTableModel bookBoardModel;
