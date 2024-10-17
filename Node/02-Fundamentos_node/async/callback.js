634e8bd00f4e15e6505a252d52cca28b9da7608e
Juan Schallmoser <schallmoserjuan@gmail.com>
2024-10-17 17:34:56 -0300

    Refactor asynchronous function to use callbacks and add log messages

--- Node/02-Fundamentos_node/async/callback.js
+++ Node/02-Fundamentos_node/async/callback.js
@@ -1,11 +1,20 @@
-function soyAsincrona(miCallback){
+function hola(nombre, miCallback){
     setTimeout(function(){
-        console.log('Hola, soy una función asíncrona');
+        console.log('Hola '+nombre);
         miCallback();
     }, 1000);
 }
 
+function adios(nombre, otroCallback){
+    setTimeout(function(){
+        console.log('Adios '+nombre);
+        otroCallback();
+    }, 1000);
+}
+
 console.log('Iniciando proceso...');
-soyAsincrona(function(){
+hola('Carlos', function(){
+    adios('Carlos', function(){
     console.log('Terminando proceso...');
+    });
 });
