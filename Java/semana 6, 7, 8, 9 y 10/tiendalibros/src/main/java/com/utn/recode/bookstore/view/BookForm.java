422f3d66694a099fa965a138e75d9bc18ad191b6
ivo <rossiivomartin@gmail.com>
2024-10-16 23:17:19 -0300

    clase 9 bookstore agrego agregar productos

--- Java/semana 6, 7, 8, 9 y 10/tiendalibros/src/main/java/com/utn/recode/bookstore/view/BookForm.java
+++ Java/semana 6, 7, 8, 9 y 10/tiendalibros/src/main/java/com/utn/recode/bookstore/view/BookForm.java
@@ -13,24 +13,17 @@ import java.util.List;
 @Component
 public class BookForm extends JFrame {
     BookService bookService;
-    private JPanel panel;
     private JTable bookBoard;
     private JTextField bookAuthor;
     private JTextField bookPrice;
-    private JTextField BookStock;
+    private JTextField bookStock;
     private JButton addButton;
     private JButton editButton;
     private JButton deleteButton;
     private JTextField bookName;
-    
+    private JPanel panel;
+
     private DefaultTableModel bookBoardModel;
-
-    @Autowired
-    public BookForm(BookService bookService) {
-        this.bookService = bookService;
-        format();
-    }
-
     private void format() {
         setContentPane(panel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -43,15 +36,16 @@ public class BookForm extends JFrame {
                 panelSize.height - getHeight() / 2
         );
     }
-
-    private void createUIComponents() {
-        bookBoardModel = new DefaultTableModel(0, 5);
-        String[] head = {"Id", "Libro", "Autor", "Precio", "Existencias"};
-        bookBoardModel.setColumnIdentifiers(head);
-        this.bookBoard = new JTable(bookBoardModel);
-        getAllBooks();
+    private void showMessage(String msn){
+        JOptionPane.showMessageDialog(this, msn);
     }
 
+    private void cleanForm(){
+        bookName.setText("");
+        bookAuthor.setText("");
+        bookPrice.setText("");
+        bookStock.setText("");
+    }
     private void getAllBooks() {
         bookBoardModel.setRowCount(0);
         List<Book> library = bookService.getAll();
@@ -66,5 +60,35 @@ public class BookForm extends JFrame {
             bookBoardModel.addRow(bookFrame);
         });
     }
+    private void addBook(){
+        if(bookName.getText().equals("")){
+            showMessage("text: book name");
+            bookName.requestFocusInWindow();
+            return;
+        }
+        String name = bookName.getText();
+        String author = bookAuthor.getText();
+        double price = Double.parseDouble(bookPrice.getText());
+        int stock = Integer.parseInt(bookStock.getText());
+        Book book = new Book(null, name, author, price, stock);
+        this.bookService.save(book);
+        showMessage("Se agrego el libro...");
+        cleanForm();
+        getAllBooks();
+    }
+    @Autowired
+    public BookForm(BookService bookService) {
+        this.bookService = bookService;
+        createUIComponents();
+        format();
+        addButton.addActionListener(e -> addBook());
+    }
 
+    private void createUIComponents() {
+        bookBoardModel = new DefaultTableModel(0, 5);
+        String[] head = {"Id", "Libro", "Autor", "Precio", "Existencias"};
+        bookBoardModel.setColumnIdentifiers(head);
+        this.bookBoard = new JTable(bookBoardModel);
+        getAllBooks();
+    }
 }
