541782e63b889a566951dc009bb6e28350abfa75
Juan Schallmoser <schallmoserjuan@gmail.com>
2024-10-17 17:40:25 -0300

    Refactor asynchronous function to use a callback and add log messages

---
+++ Node/02-Fundamentos_node/async/callbackHell.js
@@ -0,0 +1,29 @@
+function hola(nombre, miCallback){
+    setTimeout(function(){
+        console.log('Hola '+nombre);
+        miCallback();
+    }, 1000);
+}
+
+function hablar(callbackHablar){
+    setTimeout(function(){
+        console.log('Bla bla bla bla...');
+        callbackHablar();
+    }, 1000);
+}
+
+function adios(nombre, otroCallback){
+    setTimeout(function(){
+        console.log('Adios '+nombre);
+        otroCallback();
+    }, 1500);
+}
+
+// --Proceso principal
+console.log('Iniciando proceso...');
+
+hola('Carlos', function(){
+    adios('Carlos', function(){
+        console.log('Terminando proceso...');
+    });
+});
