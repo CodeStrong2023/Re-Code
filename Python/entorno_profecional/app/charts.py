f0ac52ec11749710e57031d1b86e499daad4922e
ivo <rossiivomartin@gmail.com>
2024-10-22 01:29:20 -0300

    entono profecional calse 4 app ejecucion

--- Python/entorno_profecional/app/charts.py
+++ Python/entorno_profecional/app/charts.py
@@ -1,17 +1,19 @@
 import matplotlib.pyplot as plt
 
 
-def generate_bar_chart(name, labels, values):
+def generate_bar_chart(labels, values):
     fig, ax = plt.subplots()
     ax.bar(labels, values)
-    plt.show()
+    plt.savefig("bar.png")
+    plt.close()
 
 
 def generate_pie_chart(labels, values):
     fig, ax = plt.subplots()
     ax.pie(values, labels=labels)
     ax.axis('equal')
-    plt.show()
+    plt.savefig("pie.png")
+    plt.close()
 
 
 if __name__ == '__main__':
