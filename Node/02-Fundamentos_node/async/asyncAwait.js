8f24ad46ba3b330cc5d9284c0bb77e17db693bb8
Juan Schallmoser <schallmoserjuan@gmail.com>
2024-10-31 17:00:14 -0300

    Refactor asynchronous function to use promises for adios function and add error handling

--- Node/02-Fundamentos_node/async/asyncAwait.js
+++ Node/02-Fundamentos_node/async/asyncAwait.js
@@ -7,7 +7,7 @@ async function hola(nombre){
     })
 }
 
-function hablar(nombre){
+async function hablar(nombre){
     return new Promise((resolve, reject) => {
         setTimeout(function(){
             console.log('Bla bla bla bla...');
@@ -16,20 +16,24 @@ function hablar(nombre){
     });
 }
 
-function adios(nombre){
+async function adios(nombre){
     return new Promise((resolve, reject) => {
         setTimeout(function(){
             console.log('Adios '+nombre);
             // resolve();
-            reject('Hay un error');
-        }, 1500);
+            resolve();
+        }, 1000);
     });
 }
 
 // await hola('Carlos'); mala sintaxis
 
 async function main(){
-    await hola('Carlos');
+    let nombre = await hola('Carlos');
+    await hablar();
+    await hablar();
+    await hablar();
+    await adios(nombre);
 }
 
 main();
