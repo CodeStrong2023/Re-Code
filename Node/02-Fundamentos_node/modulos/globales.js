44d82ac358dd43ace02973d5134fed29ceb0e637
Juan Schallmoser <schallmoserjuan@gmail.com>
2024-10-31 17:08:47 -0300

    Refactor globales.js to use promises for asynchronous functions and add error handling

---
+++ Node/02-Fundamentos_node/modulos/globales.js
@@ -0,0 +1,9 @@
+console.log() // Muestra en consola el mensaje que se le pase como argumento
+
+console.error() // Muestra en consola un mensaje de error
+
+setTimeout(() => {}) // Ejecuta una función después de un tiempo determinado
+
+setInterval(() => {}) // Ejecuta una función cada cierto tiempo
+
+setImmdiate(() => {}) // Ejecuta una función al final de la ejecución del event loop actual
