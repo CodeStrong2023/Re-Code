ec786fc1b60bed1e99dc8f6c575bec5c171ac3a9
ivo <rossiivomartin@gmail.com>
2024-11-04 22:52:22 -0300

    clase 11 entorno profesional fast api

--- Python/webServer/main.py
+++ Python/webServer/main.py
@@ -1,4 +1,21 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get('/')
+def get_list():
+    return [1, 2, 3]
+
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+    <h1>hi, I am a website</h1>
+    <p> hi, I am a paragraph for you to read </p>
+    """
 
 
 def run():
